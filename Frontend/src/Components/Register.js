10cda27b2edf8d0c04a60c54152d2883e37ca597
Krittanai007 <krittanai007@gmail.com>
2021-04-28 01:27:21 +0700

    Finished Register

--- Frontend/src/Components/Register.js
+++ Frontend/src/Components/Register.js
@@ -1,14 +1,16 @@
-import { makeStyles } from "@material-ui/core/styles";
-import { TextField, Typography, Button } from "@material-ui/core";
 import App from "../App";
 import React, { useCallback, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { TextField, Typography, Button } from "@material-ui/core";
+import { Link as RouterLink } from "react-router-dom";
+import Link from "@material-ui/core/Link";
 import { useHistory } from "react-router";
 import { useMutation } from "@apollo/client";
 import { CREATE_USER_MUTATION } from "../graphql/createUserMutation";
+import { ME_QUERY } from "../graphql/meQuery";
 
 const useStyles = makeStyles((s) => ({
   loginBox: {
-    height: "60vh",
     backgroundColor: "#D8BFD8",
     display: "flex",
     justifyContent: "center",
@@ -24,10 +26,11 @@ const useStyles = makeStyles((s) => ({
   form: {
     display: "contents",
   },
-  
+
   titleLogin: {
     width: "80%",
     marginBottom: "2.5%",
+    marginTop: "2.5%",
   },
 }));
 const Register = () => {
@@ -36,6 +39,9 @@ const Register = () => {
   const [name, setName] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const [tel, setTel] = useState("");
   const [createUser] = useMutation(CREATE_USER_MUTATION);
   const handleNameChange = useCallback((e) => {
     setName(e.target.value);
@@ -46,25 +52,37 @@ const Register = () => {
   const handlePasswordChange = useCallback((e) => {
     setPassword(e.target.value);
   }, []);
+  const handleAddressChange = useCallback((e) => {
+    setAddress(e.target.value);
+  }, []);
+  const handleEmailChange = useCallback((e) => {
+    setEmail(e.target.value);
+  }, []);
+  const handleTelChange = useCallback((e) => {
+    setTel(e.target.value);
+  }, []);
   const handleRegister = useCallback(
     async (e) => {
       e.preventDefault();
       try {
         const variables = {
-          record: { createUser, name, username, password },
+          record: { createUser, name, username, password, address, email, tel },
         };
         await createUser({ variables });
         setName("");
         setUsername("");
         setPassword("");
+        setAddress("");
+        setEmail("");
+        setTel("");
         history.push("/login");
-        alert("Register Successfully");
+        alert("You Can Login Now!");
       } catch (err) {
         console.log(err);
-        alert("Register Failed!!!");
+        alert("Try Again!");
       }
     },
-    [createUser, history, name, username, password]
+    [createUser, history, name, username, password, address, email, tel]
   );
   return (
     <div>
@@ -100,6 +118,33 @@ const Register = () => {
             required
             value={password}
             onChange={handlePasswordChange}
+          />
+          <TextField
+              className={classes.input}
+              label="Address"
+              variant="filled"
+              type="text"
+              value={address}
+              onChange={handleAddressChange}
+              required
+          />
+           <TextField
+              className={classes.input}
+              label="Email"
+              variant="filled"
+              type="text"
+              value={email}
+              onChange={handleEmailChange}
+              required
+          />
+          <TextField
+              className={classes.input}
+              label="Tel"
+              variant="filled"
+              type="text"
+              value={tel}
+              onChange={handleTelChange}
+              required
           />
           <Button variant="contained" color="primary" type="submit">
             Sign up
